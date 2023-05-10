c43f3952a95d39b707024ef82c85b0bff401cbb3
Jake7208 <jake7208@gmail.com>
2023-05-09 22:50:34 -0600

    tailwind finally fixed

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-3xl font-bold text-center">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		/* Trust Don't know why it throws this dumb error it works lol! */
+		background-color: theme(colors.gray.800);
+	}
+</style>
