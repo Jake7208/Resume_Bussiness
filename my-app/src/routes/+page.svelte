69fa114a194a0577ee513433f5c5a11307310886
Jake7208 <jake7208@gmail.com>
2023-05-10 22:29:18 -0600

    svelte updated styles

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,54 +1,76 @@
 <script>
-	console.log("hello");
-</script>
+
+  </script>
+  
 <html lang="en">
 		<title>The html & Tailwind for my webpage</title>
 		<!-- from-emerald-800 to-gray-950 cool gradient -->
-		<div class="flex lg:gap-96 md:gap-4 h-screen">
+		<div class="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-center h-screen" style="display: flex; align-items: center;">
+			<div class="mx-20">
+			<div class="hidden sm:block ">
+			  <h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold leading-tight mb-2 text-emerald-400">
+				"Create a Professional Resume for Free with Our Easy-to-Use Builder"
+			  </h1>
+			  <p class="text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8">
+				Welcome to our free to use resume site! We're thrilled to have you here. Whether you're just starting out in your career or looking to make a change, we're here to help you create a standout resume that showcases your skills and experience. With our easy-to-use resume builder, you can quickly and easily create a professional-looking resume that highlights your strengths and catches the eye of potential employers. Plus, our site is completely free to use, so you can create and download as many resumes as you need without worrying about any hidden fees or charges. So go ahead and get started on your new resume today - we can't wait to see what you create!
+				
+			</div> 
+			</div>
+		</div>
 
-			<div class="rounded-lg overflow-hidden mt-5 h-3/4 w-1/2 shadow-xl shadow-emerald-500/50">
-				<div class="card rounded-none ">
-					<div class="text-gray-400 text-sm font-mono p-4" style="font-size: 1em; line-height: 1.5">
-						<h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold leading-tight mb-2 text-emerald-400 ">
-							Let's build a resume
-						</h1>
-						<p class="text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8">
-							Welcome to our free to use resume site! We're thrilled to have you here. Whether you're just 
-							starting out in your career or looking to make a change, we're here to help you create a standout 
-							resume that showcases your skills and experience. With our easy-to-use resume builder, you can quickly
-							and easily create a professional-looking resume that highlights your strengths and catches the eye of 
-							potential employers. Plus, our site is completely free to use, so you can create and download as many
-							resumes as you need without worrying about any hidden 
-							fees or charges. 
-							
-							So go ahead and get started on your new resume today - we can't wait to see what you create!</p>
-						</div>
+
+		<div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 mb-10">
+			<!-- card 1 -->
+			<div class="bg-gray-800 rounded-2xl overflow-hidden mt-5 h-96 w-full shadow-lg shadow-gray-900/100">
+				<div class="card">
+					<div class="flex justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-48 h-48">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+						  </svg>
+						  
+					  </div>
+					<div class="text-gray-500 text-sm font-mono p-4" style="font-size: 1em; line-height: 1.5">
+						<p class="text-center text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8">
+							Create a standout resume that highlights your skills and experience.
+						</p>
 					</div>
 				</div>
-				
-				<div class="flex mt-10 h-3/4 lg:w-1/4 w-1/2 justify-center items-center " style="
-				<!-- border: 1px solid; -->
-				">
-				<div class="h-1/3 rounded p-2" >
-					<!-- Google login  button un linked  -->
-					<button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 dark:hover:bg-gradient-to-r from-cyan-500 to-gray-800 dark:hover:shadow-lg shadow-cyan-500/50 mr-2 mb-2">
-						<svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
-						Sign in with Google
-					</button>
-					<!-- git hub login button  un linked -->
-					<button type="button" class="text-white bg-[#632A90] hover:bg-[#632A90]/90 focus:ring-4 focus:outline-none focus:ring-[#632A90]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-gradient-to-r from-[#632A90] to-blue-500 ease-in delay-150 mr-2 mb-2">
-						<svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
-						Sign in with Github
-					</button>
-					<button type="button" class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-gradient-to-r from-gray-800 to-blue-500 ease-in delay-150 mr-2 mb-2">
-						<svg class="w-4 h-4 mr-2 -ml-1 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
-						Continue as Guest
-					</button>
-				</div>		
+			</div>
+			<!-- card 2 -->
+			<div class="bg-gray-800 rounded-2xl overflow-hidden mt-5 h-96 w-full shadow-lg shadow-gray-900/100">
+				<div class="card rounded-none">
+					<div class="flex justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-48 h-48">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+						  </svg>
+					</div>
+					  
+				  <div class="text-gray-400 text-sm font-mono p-4" style="font-size: 1em; line-height: 1.5">
+					<p class="text-center text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8">
+					  Craft professional resumes effortlessly with our user-friendly resume builder.
+					</p>
+				  </div>
+				</div>
+			  </div>
+		  
+					<!-- card 3 -->
+					<div class="bg-gray-800 rounded-2xl overflow-hidden mt-5 h-96 w-full shadow-lg shadow-gray-900/100">
+						<div class="card rounded-none">
+					<div class="flex justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-48 h-48">
+						  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+						</svg>
+					  </div>
+					  
+					  
+					<div class="text-gray-400 text-sm font-mono p-4" style="font-size: 1em; line-height: 1.5">
+						<p class="text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8">
+							"Effortlessly create standout resumes that highlight your strengths and experience."
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>	
-			<!-- </div> -->
-		<!-- </div> -->
 
 </html>
 
