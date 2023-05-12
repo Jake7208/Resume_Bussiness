510b8f505f078c2da962ff3d8bdef75ce785884b
Jake7208 <jake7208@gmail.com>
2023-05-12 03:14:55 -0600

    frontend update

---
+++ my-app/src/components/Contributers.svelte
@@ -0,0 +1,14 @@
+<div class="bg-white rounded-3xl overflow-hidden h-96 w-full shadow-lg shadow-gray-900/100">
+	<div class="card">
+		<!-- <div class="flex justify-center"> -->
+		<slot name="icon" />
+		<!-- </div> -->
+		<div class="text-gray-500 text-sm font-mono p-4" style="font-size: 1em; line-height: 1.5">
+			<p
+				class="text-center text-sm sm:text-base md:text-lg lg:text-lg xl:text-lg leading-relaxed text-gray-300 indent-8"
+			>
+				<slot />
+			</p>
+		</div>
+	</div>
+</div>
--- my-app/tailwind.config.cjs
+++ my-app/tailwind.config.cjs
@@ -1,12 +1,23 @@
-// /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
 	purge: ['./src/**/*.svelte', './src/**/*.css'],
 	// content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			gridTemplateColumns: {
+				2: 'repeat(2, minmax(0, 1fr))'
+			},
 			fontFamily: {
 				inter: ['Inter', 'sans-serif']
 			}
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
 		}
 	},
 	variants: {
